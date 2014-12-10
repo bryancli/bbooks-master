3dbcdc49312df38ec70a09a699b503acd3ec53b0
Bryan <bryancli@gmail.com>
2014-12-10 00:27:21 -0600

    V6: VFFF

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,5 +25,5 @@ class ApplicationController < ActionController::Base
 	  #     	## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
    #  	end
   	# end
- 	
+
 end
